99c470329d50e95332d2f9e7e9ec0c1f4290ead9
Jackie <44011334mm@stu.gdou.edu.cn>
2024-08-02 11:44:28 +0800

    update 'app.py'

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from lmdeploy.messages import TurbomindEngineConfig
 # 获取个人 token
 git_token = os.getenv('GIT_READ_TOKEN')
 # 下载微调后的模型
-base_path = './ft-model'
-os.system(f'git clone https://ZK-Jackie:{git_token}@code.openxlab.org.cn/ZK-Jackie/my-internlm.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
+base_path = os.path.dirname(__file__)
+model_path = f'{base_path}/ft-model'
+os.system(f'git clone https://ZK-Jackie:{git_token}@code.openxlab.org.cn/ZK-Jackie/my-internlm.git {model_path}')
+os.system(f'cd {model_path} && git lfs pull')
 # 部署模型
-model_path = f'{__file__}/ft-model'
 run_local(model_path, server_port=7860)
